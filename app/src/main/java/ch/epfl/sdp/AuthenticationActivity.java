1dbd76010177800828a606932091fce18c72e708
TheCrazyKing <lucas.meier@epfl.ch>
2020-02-29 20:26:44 +0100

    Add signIn button and functionality

--- app/src/main/java/ch/epfl/sdp/AuthenticationActivity.java
+++ app/src/main/java/ch/epfl/sdp/AuthenticationActivity.java
@@ -1,14 +1,75 @@
 package ch.epfl.sdp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AuthenticationActivity extends AppCompatActivity{
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+/**
+ *  AuthenticationActivity : handling the signIn process via google play
+ *
+ *    -> this class is always triggered when beginning the app
+ */
+public class AuthenticationActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final int RC_SIGN_IN = 1; //any number, but common for the app
+    protected GoogleSignInClient gsc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient gsc = GoogleSignIn.getClient(this, gso);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if(account == null){
+            //display the Google Sign-in button -> not yet registered
+
+        }else{
+            // hide the sign-in button, launch your main activity -> already registered
+            Intent intent = new Intent(this, MainActivity.class);// New activity
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //clears this activity's stack
+            startActivity(intent);
+            finish(); // Launches MainActivity
+        }
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+    private void signIn() {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 }
 
 

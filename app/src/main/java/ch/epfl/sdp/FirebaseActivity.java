4b2bee519f0f70439a37b9bea240c9ac4810dd50
TheBlueHawk <kblin@outlook.com>
2020-03-05 23:35:35 +0100

    AddUser() and ReadData() implemented

--- app/src/main/java/ch/epfl/sdp/FirebaseActivity.java
+++ app/src/main/java/ch/epfl/sdp/FirebaseActivity.java
@@ -1,12 +1,36 @@
 package ch.epfl.sdp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.WriteBatch;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class FirebaseActivity extends AppCompatActivity {
+
+    private static final String TAG = "FirebaseActivity";
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +41,56 @@ public class FirebaseActivity extends AppCompatActivity {
         Intent intent = getIntent();
     }
 
-    
+    public void addUser(View view){
+        //Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("Name", "Ada Lovelace");
+        user.put("Age", 19);
+        user.put("Infected", false);
+
+        // Add a new document with a generated ID
+        db.collection("Players")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        TextView outputView = findViewById(R.id.FirebaseUploadConfirmation);
+                        outputView.setText(R.string.DocumentSnapshot_successfully_added);
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                TextView outputView = findViewById(R.id.FirebaseUploadConfirmation);
+                outputView.setText("Error adding the document");
+                Log.w(TAG, "Error adding document", e);
+            }
+        });
+    }
+
+    public void readData(View view){
+        db.collection("Players")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                TextView outputView = findViewById(R.id.FirebaseDownloadResult);
+                                outputView.setText(String.format("%s => %s", document.getId(), document.getData()));
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+
+
+
+
+
+
 
 }
--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -3,7 +3,6 @@ package ch.epfl.sdp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 

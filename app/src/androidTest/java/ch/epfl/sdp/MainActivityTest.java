0421d6a27562f4810b82f4b11b1da39e62452aa3
TheBlueHawk <kblin@outlook.com>
2020-03-05 14:38:42 +0100

    New testCanGoBackFromFirebaseActivity

--- app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -1,8 +1,16 @@
 package ch.epfl.sdp;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +22,9 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -32,5 +42,44 @@ public class MainActivityTest {
     public void testCanGoToFirebaseActivity() {
         onView(withId(R.id.launchButton)).perform(click());
         onView(withId(R.id.FirebaseTextView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testCanGoBackFromFirebaseActivity(){
+        onView(withId(R.id.launchButton)).perform(click());
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+        onView(withId(R.id.launchButton)).check(matches(isDisplayed()));
+    }
+
+
+
+
+    /*************************PRIVATE METHODS****************************/
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }

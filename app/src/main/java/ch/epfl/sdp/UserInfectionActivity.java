a31699274b4c9e6e727a96f37d405f2e7ae1d675
lucieho <bamboomusic0@gmail.com>
2020-03-15 19:26:10 +0100

    Change view (text & button) when button clicked

--- app/src/main/java/ch/epfl/sdp/UserInfectionActivity.java
+++ app/src/main/java/ch/epfl/sdp/UserInfectionActivity.java
@@ -3,6 +3,9 @@ package ch.epfl.sdp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class UserInfectionActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class UserInfectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_infection);
+
+        TextView infectionStatusView = findViewById(R.id.infectionStatusView);
+        Button infectionStatusButton = findViewById(R.id.infectionStatusButton);
+
+        infectionStatusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.infectionStatusButton) {
+                    String buttonText = ((Button)v).getText().toString();
+                    if (buttonText.equals("I am infected")) {
+                        infectionStatusButton.setText("I am cured");
+                        infectionStatusButton.setBackgroundTintList(getResources().getColorStateList(R.color.colorGreenCured));
+                        infectionStatusView.setText("Your user status is set to infected.");
+                    }
+                    else if (buttonText.equals("I am cured")) {
+                        infectionStatusButton.setText("I am infected");
+                        infectionStatusButton.setBackgroundTintList(getResources().getColorStateList(R.color.colorRedInfected));
+                        infectionStatusView.setText("Your user status is set to not infected.");
+                    }
+                }
+            }
+        });
     }
 }

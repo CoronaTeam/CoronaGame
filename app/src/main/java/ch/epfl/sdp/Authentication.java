ab7136700605ace9e4e915741412f35d5969f875
TheCrazyKing <lucas.meier@epfl.ch>
2020-03-17 21:51:28 +0100

    finish the branch

--- app/src/main/java/ch/epfl/sdp/Authentication.java
+++ app/src/main/java/ch/epfl/sdp/Authentication.java
@@ -86,7 +86,7 @@ public class Authentication extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...) ;
         if (requestCode == RC_SIGN_IN) {
             // The Task returned from this call is always completed, no need to attach
             // a listener.

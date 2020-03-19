d1830481a2f3ccf0ac5bfcfa3232e30a92832c21
matteopariset <matteopariset@gmail.com>
2020-03-19 22:34:29 +0100

    Add automatic upload of position from GpsActivity + (partial) tests

--- app/src/main/java/ch/epfl/sdp/Authentication.java
+++ app/src/main/java/ch/epfl/sdp/Authentication.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,15 +17,17 @@ import com.google.android.gms.tasks.Task;
 
 
 /**
- *  AuthenticationActivity : handling the signIn process via google play. This class will check if a user has been already logged in.
- *                          If not, it displays the sign In button and if this latter is pressed, a window built by google is shown.
- *                           Then, the class AccountGettingActivity manages the account information use.
- *  @author lucas
+ * AuthenticationActivity : handling the signIn process via google play. This class will check if a user has been already logged in.
+ * If not, it displays the sign In button and if this latter is pressed, a window built by google is shown.
+ * Then, the class AccountGettingActivity manages the account information use.
+ *
+ * @author lucas
  */
 public class Authentication extends AppCompatActivity {
     public static final int RC_SIGN_IN = 0; //any number, but common for the app
     GoogleSignInClient googleSignInClient;
     View signIn;// error prone line if View is replaced by Button
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,9 +40,9 @@ public class Authentication extends AppCompatActivity {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         // Build a GoogleSignInClient with the options specified by gso.
         googleSignInClient = GoogleSignIn.getClient(this, gso);
-        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 switch (v.getId()) {
                     case R.id.sign_in_button:
                         signIn();
@@ -55,7 +56,7 @@ public class Authentication extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         // Check for existing Google Sign In account, if the user is already signed in
 // the GoogleSignInAccount will be non-null.
@@ -64,10 +65,10 @@ public class Authentication extends AppCompatActivity {
     }
 
     private void updateUI(GoogleSignInAccount account) {
-        if(account == null){
+        if (account == null) {
             //display the Google Sign-in button -> not yet registered
             signIn.setVisibility(View.VISIBLE);
-        }else{
+        } else {
             // hide the sign-in button, launch your main activity -> already registered
             signIn.setVisibility(View.INVISIBLE);
 //            startActivity(new Intent(this, AccountGettingActivity.class));
@@ -82,6 +83,7 @@ public class Authentication extends AppCompatActivity {
         Intent signInIntent = googleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -92,10 +94,11 @@ public class Authentication extends AppCompatActivity {
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
-        }else{
+        } else {
             throw new IllegalStateException("Request code is not = RC_SIGN_IN");
         }
     }
+
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
@@ -103,8 +106,8 @@ public class Authentication extends AppCompatActivity {
             // Signed in successfully, show authenticated UI.
             updateUI(account);
         } catch (ApiException e) {
-            if(e.getStatusCode() == 7){
-                Toast.makeText(Authentication.this,"Please check your internet connection", Toast.LENGTH_LONG).show();
+            if (e.getStatusCode() == 7) {
+                Toast.makeText(Authentication.this, "Please check your internet connection", Toast.LENGTH_LONG).show();
             }
 
             // The ApiException status code indicates the detailed failure reason.

ed83d45fb7d9047b848b82e110e2b65d8faf3f3d
Adrien Remillieux <adrien.remillieux@epfl.ch>
2020-03-19 15:26:39 +0100

    added show other users Positions Functionality

--- app/src/main/java/ch/epfl/sdp/MapActivity.java
+++ app/src/main/java/ch/epfl/sdp/MapActivity.java
@@ -1,7 +1,13 @@
 package ch.epfl.sdp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -24,91 +30,38 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import static ch.epfl.sdp.LocationBroker.Provider.GPS;
 
 public class MapActivity extends AppCompatActivity implements LocationListener{
 
     private MapView mapView;
     private MapboxMap map;
+    private LocationBroker locationBroker;
+    private LatLng prevLocation = new LatLng(0, 0);
 
     public final static int LOCATION_PERMISSION_REQUEST = 20201;
     private static final int MIN_UP_INTERVAL_MILLISECS = 1000;
     private static final int MIN_UP_INTERVAL_METERS = 5;
 
-    private LocationBroker locationBroker;
+    private FirestoreInteractor db;
+    private QueryHandler handler;
 
-    private LatLng prevLocation = new LatLng(0, 0);
+    CircleManager positionMarkerManager;
+    Circle userPositionMarker;
+    ArrayList<Circle> otherUsersPositionMarkers;
 
-    CircleManager symbolManager;
-    Circle symbol;
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if (locationBroker.hasPermissions(GPS)) {
-            prevLocation =  new LatLng(location.getLatitude(), location.getLongitude());
-            updateMarkerPosition(prevLocation);
-            System.out.println("new location");
-        } else {
-            Toast.makeText(this, "Missing permission", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void updateMarkerPosition(LatLng location) {
-        // This method is where we update the marker position once we have new coordinates. First we
-        // check if this is the first time we are executing this handler, the best way to do this is
-        // check if marker is null
-
-        if (map != null && map.getStyle() != null) {
-            symbol.setLatLng(location);
-            symbolManager.update(symbol);
-            map.animateCamera(CameraUpdateFactory.newLatLng(location));
-        }
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-    }
-
-    private void goOnline() {
-        locationBroker.requestLocationUpdates(GPS, MIN_UP_INTERVAL_MILLISECS, MIN_UP_INTERVAL_METERS, this);
-        Toast.makeText(this, R.string.gps_status_on, Toast.LENGTH_SHORT).show();
-    }
-
-    private void goOffline() {
-        Toast.makeText(this, R.string.gps_status_off, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        if (provider.equals(LocationManager.GPS_PROVIDER)) {
-            if (locationBroker.hasPermissions(GPS)) {
-                goOnline();
-            } else {
-                locationBroker.requestPermissions(LOCATION_PERMISSION_REQUEST);
-            }
-        }
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        if (provider.equals(LocationManager.GPS_PROVIDER)) {
-            goOffline();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == LOCATION_PERMISSION_REQUEST
-                && grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            goOnline();
-        }
-    }
+    MapActivity classPointer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        classPointer = this;
+        otherUsersPositionMarkers = new ArrayList<>();
+        FirestoreWrapper firestoreWrapper = new ConcreteFirestoreWrapper(FirebaseFirestore.getInstance());
+        db = new HistoryFirestoreInteractor(firestoreWrapper);
 
         // TODO: do not execute in production code
         if (locationBroker == null) {
@@ -132,17 +85,81 @@ public class MapActivity extends AppCompatActivity implements LocationListener{
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
-                        symbolManager = new CircleManager(mapView, map, style);
+                        positionMarkerManager = new CircleManager(mapView, map, style);
 
-                        symbol = symbolManager.create(new CircleOptions()
+                        userPositionMarker = positionMarkerManager.create(new CircleOptions()
                                 .withLatLng(prevLocation));
 
                         updateMarkerPosition(prevLocation);
+                        initQueryHandler();
+                        db.read(handler);
                     }
 
                 });
             }
         });
+    }
+
+    private void initQueryHandler() {
+
+        handler = new QueryHandler() {
+            @Override
+            public void onSuccess(QuerySnapshot snapshot) {
+
+                Iterator<QueryDocumentSnapshot> qsIterator = snapshot.iterator();
+                Iterator<Circle> pmIterator = otherUsersPositionMarkers.iterator();
+                while (pmIterator.hasNext()){
+                    if(qsIterator.hasNext()){
+                        QueryDocumentSnapshot qs = qsIterator.next();
+                        Circle pm = pmIterator.next();
+                        pm.setLatLng(new LatLng(((GeoPoint)(qs.get("Position"))).getLatitude(),
+                                ((GeoPoint)(qs.get("Position"))).getLongitude()));
+                    }
+                    else{
+                        positionMarkerManager.delete(pmIterator.next());
+                        pmIterator.remove();
+                    }
+                }
+                while (qsIterator.hasNext()){
+                    QueryDocumentSnapshot qs = qsIterator.next();
+                    Circle pm = positionMarkerManager.create(new CircleOptions()
+                            .withLatLng(new LatLng(
+                                    ((GeoPoint)(qs.get("Position"))).getLatitude(),
+                                    ((GeoPoint)(qs.get("Position"))).getLongitude()))
+                            .withCircleColor("#1703fc")
+                    );
+                    otherUsersPositionMarkers.add(pm);
+                }
+            }
+
+            @Override
+            public void onFailure() {
+                Toast.makeText(classPointer, "Cannot retrieve positions from database", Toast.LENGTH_LONG).show();
+            }
+        };
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (locationBroker.hasPermissions(GPS)) {
+            prevLocation =  new LatLng(location.getLatitude(), location.getLongitude());
+            updateMarkerPosition(prevLocation);
+            System.out.println("new location");
+        } else {
+            Toast.makeText(this, "Missing permission", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void updateMarkerPosition(LatLng location) {
+        // This method is where we update the marker position once we have new coordinates. First we
+        // check if this is the first time we are executing this handler, the best way to do this is
+        // check if marker is null
+
+        if (map != null && map.getStyle() != null) {
+            userPositionMarker.setLatLng(location);
+            positionMarkerManager.update(userPositionMarker);
+            map.animateCamera(CameraUpdateFactory.newLatLng(location));
+        }
     }
 
     // Add the mapView lifecycle to the activity's lifecycle methods
@@ -188,6 +205,47 @@ public class MapActivity extends AppCompatActivity implements LocationListener{
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    private void goOnline() {
+        locationBroker.requestLocationUpdates(GPS, MIN_UP_INTERVAL_MILLISECS, MIN_UP_INTERVAL_METERS, this);
+        Toast.makeText(this, R.string.gps_status_on, Toast.LENGTH_SHORT).show();
+    }
+
+    private void goOffline() {
+        Toast.makeText(this, R.string.gps_status_off, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        if (provider.equals(LocationManager.GPS_PROVIDER)) {
+            if (locationBroker.hasPermissions(GPS)) {
+                goOnline();
+            } else {
+                locationBroker.requestPermissions(LOCATION_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        if (provider.equals(LocationManager.GPS_PROVIDER)) {
+            goOffline();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST
+                && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            goOnline();
+        }
     }
 
     @Override

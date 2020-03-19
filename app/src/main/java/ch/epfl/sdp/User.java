bbcf1d225cb52f2b41689266cc9909de28f6840c
lucieho <bamboomusic0@gmail.com>
2020-03-19 19:47:29 +0100

    add TAG for Logs in Logcat window (UserInfectionActivity)

--- app/src/main/java/ch/epfl/sdp/User.java
+++ app/src/main/java/ch/epfl/sdp/User.java
@@ -1,24 +1,17 @@
 package ch.epfl.sdp;
 
 import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
-
 import java.util.HashMap;
 import java.util.Map;
-
-import static android.content.ContentValues.TAG;
 
 public class User {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private Map<String, Object> user = new HashMap<>();
+    private static final String TAG = "UserInfectionActivity";
 
     public User(String userName, int userAge, boolean infected) {
         user.put("Name", userName);

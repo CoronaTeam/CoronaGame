c423a1ed577c9e81be399f7d2b08a559318d3ce0
TheBlueHawk <kblin@outlook.com>
2020-03-20 18:11:41 +0100

    First fingerprint enabled
    If not present the app proceed as before

--- app/src/main/java/ch/epfl/sdp/UserInfectionActivity.java
+++ app/src/main/java/ch/epfl/sdp/UserInfectionActivity.java
@@ -1,12 +1,18 @@
 package ch.epfl.sdp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.Executor;
 
 public class UserInfectionActivity extends AppCompatActivity {
 
@@ -16,6 +22,11 @@ public class UserInfectionActivity extends AppCompatActivity {
     private User user = new User("Test", User.DEFAULT_FAMILY_NAME, User.DEFAULT_EMAIL,
             User.DEFAULT_URI, User.DEFAULT_PLAYERID, User.DEFAULT_USERID, 25, false);
     private String userName = "Test"; // TODO: this is temporary: we need to get the real current user
+
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +40,54 @@ public class UserInfectionActivity extends AppCompatActivity {
         infectionStatusView.setSaveEnabled(true);
         infectionStatusButton.setSaveEnabled(true);
 
+        executor = ContextCompat.getMainExecutor(this);
+
+        biometricPrompt = new BiometricPrompt(UserInfectionActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Use account password")
+                .build();
+
         infectionStatusButton.setOnClickListener(v -> {
             CharSequence buttonText = ((Button)v).getText();
             if (buttonText.equals(getResources().getString(R.string.i_am_infected))) {
+                biometricPrompt.authenticate(promptInfo);
                 clickAction(infectionStatusButton, infectionStatusView, R.string.i_am_cured,
                         R.string.your_user_status_is_set_to_infected, R.color.colorGreenCured);
                 //upload to firebase
                 user.modifyUserInfectionStatus(userName, true,
                         value -> infectionUploadView.setText(value));
-
-
             }
             else {
+                biometricPrompt.authenticate(promptInfo);
                 clickAction(infectionStatusButton, infectionStatusView, R.string.i_am_infected,
                         R.string.your_user_status_is_set_to_not_infected, R.color.colorRedInfected);
                 //upload to firebase
@@ -49,6 +96,8 @@ public class UserInfectionActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     private void clickAction(Button button, TextView textView, int buttonText, int textViewText, int buttonColor) {
         button.setText(buttonText);

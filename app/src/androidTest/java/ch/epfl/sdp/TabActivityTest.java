8556227d292495e4e9deeba113eddc2efa44b70b
ulydev <ulydev@live.fr>
2020-03-20 15:01:42 +0100

    fix tabActivity tests

--- app/src/androidTest/java/ch/epfl/sdp/TabActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/TabActivityTest.java
@@ -6,14 +6,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasFocus;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class TabActivityTest {
@@ -30,8 +35,14 @@ public class TabActivityTest {
 
     @Test
     public void testTabsDisplayCorrectly() {
-        onView(withId(R.id.map_fragment)).check(matches(isDisplayed()));
-        onView(withId(R.id.history_fragment)).check(matches(isDisplayed()));
+        onView(withId(R.id.mapView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testTabsMoveCorrectly() {
+        onView(withId(R.id.history_tracker)).check(matches(not(hasFocus())));
+        onView(withText(mActivityRule.getActivity().getString(R.string.tab_history))).perform(click());
+        onView(withId(R.id.history_tracker)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
 }

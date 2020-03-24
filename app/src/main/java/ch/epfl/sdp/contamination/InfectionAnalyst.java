bfec8fb8bee88c4f2b1a310857140edfd8087348
TheCrazyKing <lucas.meier@epfl.ch>
2020-03-24 13:50:13 +0100

    add 2 constants

--- app/src/main/java/ch/epfl/sdp/contamination/InfectionAnalyst.java
+++ app/src/main/java/ch/epfl/sdp/contamination/InfectionAnalyst.java
@@ -3,9 +3,10 @@ package ch.epfl.sdp.contamination;
 import java.util.Date;
 
 public interface InfectionAnalyst {
-    int radius = 3;//maximum radius up to which infection may happen
+    int RADIUS = 3;//maximum radius up to which infection may happen
+    int WINDOW_FOR_INFECTION_DETECTION = 1200000; //[ms] Window of time during which a user should not meet a person to much to stay fit. actual : 20 min
     /**
-     * Returns the probability of infection during WINDOW_FOR_LOCATION_AGGREGATION amount of time, starting at startTime
+     * Returns the probability of infection during WINDOW_FOR_INFECTION_DETECTION amount of time, starting at startTime
      * @param startTime
      */
     void getProbabilityOfInfection(Date startTime);
--- app/src/main/java/ch/epfl/sdp/contamination/PositionAggregator.java
+++ app/src/main/java/ch/epfl/sdp/contamination/PositionAggregator.java
@@ -9,7 +9,7 @@ A positionAggregator should take the average value of multiple points in space d
 Then, is rounds this average to the nearest meter (i.e. rounding latitude/longitude to 5 decimals), and sends this location along with the last time to a DataSender.
  */
 public interface PositionAggregator {
-    int WINDOW_FOR_LOCATION_AGGREGATION = 300000; //[ms] window of time in which you should not meet a person to much times. actual : 5min
+    int WINDOW_FOR_LOCATION_AGGREGATION = 300000; //[ms] This is the frequency with which the (mean) position will be uploaded. actual : 5min
 
     /**
      *

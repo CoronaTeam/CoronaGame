b642cc89f67125a52886022000b9619108a82238
TheBlueHawk <kblin@outlook.com>
2020-03-31 23:23:04 +0200

    additionals MainActivityTests

--- app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -3,13 +3,17 @@ package ch.epfl.sdp;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.repeatedlyUntil;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
@@ -41,4 +45,32 @@ public class MainActivityTest {
         onView(withId(UIelementID)).check(matches(isDisplayed()));
     }
 
+    @Test
+    public void testCanGoToUserAuthActivity() {
+        clickAndCheck(R.id.launch_SignIn_Button, R.id.sign_in_button);
+    }
+
+    @Test
+    public void testCanGoToHistoryActivity() {
+        clickAndCheck(R.id.launch_history, R.id.history_fragment);
+    }
+
+    @Test
+    public void testCanGoToTabActivity() {
+        scrollAndWait(R.id.button_tabs);
+        clickAndCheck(R.id.button_tabs, R.id.pager);
+    }
+
+    @Test
+    @Ignore("Issue with scrolling: button not 100% displayed")
+    public void testCanGoToUserInfectionActivity() {
+        scrollAndWait(R.id.button_user_infection);
+        clickAndCheck(R.id.button_user_infection, R.id.infectionStatusButton);
+    }
+
+    private void scrollAndWait(int id) {
+        onView(withId(R.id.scrollView2)).perform(repeatedlyUntil(swipeUp(),
+                hasDescendant(withId(id)),
+                10));
+    }
 }

085577e2f979d3b69b2424f269ae6d5b5601a4a5
ulydev <ulydev@live.fr>
2020-03-31 19:23:10 +0200

    remove signOut and attributes from AccountFragment

--- app/src/main/java/ch/epfl/sdp/fragment/AccountFragment.java
+++ app/src/main/java/ch/epfl/sdp/fragment/AccountFragment.java
@@ -1,7 +1,5 @@
 package ch.epfl.sdp.fragment;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,31 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import ch.epfl.sdp.Account;
-import ch.epfl.sdp.AccountFactory;
-import ch.epfl.sdp.AccountGetting;
-import ch.epfl.sdp.Authentication;
+import ch.epfl.sdp.AuthenticationManager;
 import ch.epfl.sdp.R;
-import ch.epfl.sdp.User;
 
 public class AccountFragment extends Fragment {
 
-    private GoogleSignInClient mGoogleSignInClient;
-    private Account accountInUse;
     private TextView name;
     private TextView email;
     private TextView lastName;
@@ -54,9 +40,7 @@ public class AccountFragment extends Fragment {
         lastName = view.findViewById(R.id.lastName);
         userIdView = view.findViewById(R.id.userIdView);
         img = view.findViewById(R.id.profileImage);
-        mGoogleSignInClient = AccountGetting.getGoogleClient(getActivity());
-        accountInUse = AccountGetting.getAccount(getActivity());
-        getAndShowAccountInfo(accountInUse);
+        getAndShowAccountInfo(AuthenticationManager.getAccount(getActivity()));
 
         return view;
     }
@@ -89,26 +73,7 @@ public class AccountFragment extends Fragment {
     }
 
     public void signOut(View v) {
-        if (accountInUse.isGoogle()) {
-            mGoogleSignInClient.signOut()
-                    .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            Toast.makeText(getActivity(), "Signed out successfully!", Toast.LENGTH_LONG).show();
-                            Intent intent = new Intent(getActivity(), Authentication.class);// New activity
-//                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //clears this activity's stack
-                            startActivity(intent);
-                            getActivity().finish();
-                        }
-                    });
-        } else {
-            //no need to sign out from google, just go to the other activity
-            // do not toast during test !
-            Intent intent = new Intent(getActivity(), Authentication.class);// New activity
-            startActivity(intent);
-            getActivity().finish();
-        }
-
+        AuthenticationManager.signOut(getActivity());
     }
 
 }

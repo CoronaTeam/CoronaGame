145ce09335ad7178606bdeec9247ad470299ad17
lucieho <bamboomusic0@gmail.com>
2020-04-17 11:45:24 +0200

    remove redundant functions setView & statusButtonAction + replace usage of setView + modify setInfectionColorMessage + regroup biometrics function all together

--- app/src/main/java/ch/epfl/sdp/UserInfectionActivity.java
+++ app/src/main/java/ch/epfl/sdp/UserInfectionActivity.java
@@ -17,8 +17,6 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -109,25 +107,65 @@ public class UserInfectionActivity extends AppCompatActivity {
         userName = account.getDisplayName();
         userNameView.setText(userName);
         retrieveUserInfectionStatus(
-                value -> setView(infectionStatusButton, infectionStatusView, value));
+                this::setInfectionColorAndMessage);
     }
 
-    private void statusButtonAction(Button b) {
-        checkOnline();
-        CharSequence buttonText = b.getText();
+    private void executeHealthStatusChange() {
+        CharSequence buttonText = infectionStatusButton.getText();
         if (buttonText.equals(getResources().getString(R.string.i_am_infected))) {
-            setView(infectionStatusButton, infectionStatusView, true);
-            //upload to firebase
-            modifyUserInfectionStatus(userName, true,
-                    value -> infectionUploadView.setText(value));
-
-
+            setInfectionColorAndMessage(true);
         } else {
-            setView(infectionStatusButton, infectionStatusView, false);
-            //upload to firebase
-            modifyUserInfectionStatus(userName, false,
-                    value -> infectionUploadView.setText(value));
+            setInfectionColorAndMessage(false);
         }
+    }
+
+    public void modifyUserInfectionStatus(String userPath, Boolean infected, Callback<String> callback) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("Infected", infected);
+        db.collection("Users").document(userPath)
+                .set(user, SetOptions.merge());
+
+        DocumentReference userRef = db.collection("Users").document(userPath);
+
+        userRef
+                .update("Infected", infected)
+                .addOnSuccessListener(documentReference ->
+                        callback.onCallback("User infection status successfully updated!"))
+                .addOnFailureListener(e ->
+                        callback.onCallback("Error updating user infection status."));
+    }
+
+    public void retrieveUserInfectionStatus(Callback<Boolean> callbackBoolean) {
+        db.collection("Users").document(userName).get().addOnSuccessListener(documentSnapshot ->
+        {
+            Log.d(TAG, "Infected status successfully loaded.");
+            Object infected = documentSnapshot.get("Infected");
+            if (infected == null) {
+                callbackBoolean.onCallback(false);
+            } else {
+                callbackBoolean.onCallback((boolean) infected);
+            }
+        })
+                .addOnFailureListener(e ->
+                        Log.w(TAG, "Error retrieving infection status from Firestore.", e));
+    }
+
+    private void setInfectionColorAndMessage(boolean infected) {
+        int buttonTextID = infected ? R.string.i_am_cured : R.string.i_am_infected;
+        int messageID = infected ? R.string.your_user_status_is_set_to_infected :
+                R.string.your_user_status_is_set_to_not_infected;
+        int colorID = infected ? R.color.colorRedInfected : R.color.colorGreenCured;
+        clickAction(infectionStatusButton, infectionStatusView, buttonTextID,
+                messageID, colorID);
+        modifyUserInfectionStatus(userName, infected,
+                value -> infectionUploadView.setText(String.format("%s at %s", value,
+                        Calendar.getInstance().getTime())));
+    }
+
+    private void clickAction(Button button, TextView textView, int buttonText, int textViewText, int textColor) {
+        button.setText(buttonText);
+        textView.setTextColor(getResources().getColorStateList(textColor, this.getTheme()));
+        textView.setText(textViewText);
     }
 
     private BiometricPromptWrapper biometricPromptBuilder(Executor executor) {
@@ -165,58 +203,6 @@ public class UserInfectionActivity extends AppCompatActivity {
                 .build();
     }
 
-    private void executeHealthStatusChange() {
-        CharSequence buttonText = infectionStatusButton.getText();
-        if (buttonText.equals(getResources().getString(R.string.i_am_infected))) {
-            setInfectionColorAndMessage(R.string.i_am_cured,
-                    R.string.your_user_status_is_set_to_infected, R.color.colorRedInfected, true);
-        } else {
-            setInfectionColorAndMessage(R.string.i_am_infected,
-                    R.string.your_user_status_is_set_to_not_infected, R.color.colorGreenCured,
-                    false);
-        }
-    }
-
-    public void modifyUserInfectionStatus(String userPath, Boolean infected, Callback<String> callback) {
-        Map<String, Object> user = new HashMap<>();
-        user.put("Infected", infected);
-        db.collection("Users").document(userPath)
-                .set(user, SetOptions.merge());
-
-        DocumentReference userRef = db.collection("Users").document(userPath);
-
-        userRef
-                .update("Infected", infected)
-                .addOnSuccessListener(documentReference ->
-                        callback.onCallback("User infection status successfully updated!"))
-                .addOnFailureListener(e ->
-                        callback.onCallback("Error updating user infection status."));
-    }
-
-    public void retrieveUserInfectionStatus(Callback<Boolean> callbackBoolean) {
-        db.collection("Users").document(userName).get().addOnSuccessListener(documentSnapshot ->
-        {
-            Log.d(TAG, "Infected status successfully loaded.");
-            Object infected = documentSnapshot.get("Infected");
-            if (infected == null) {
-                callbackBoolean.onCallback(false);
-            } else {
-                callbackBoolean.onCallback((boolean) infected);
-            }
-        })
-                .addOnFailureListener(e ->
-                        Log.w(TAG, "Error retrieving infection status from Firestore.", e));
-    }
-
-    private void setInfectionColorAndMessage(int buttonTextID, int messageID, int colorID,
-                                             boolean infected) {
-        clickAction(infectionStatusButton, infectionStatusView, buttonTextID,
-                messageID, colorID);
-        modifyUserInfectionStatus(userName, infected,
-                value -> infectionUploadView.setText(String.format("%s at %s", value,
-                        Calendar.getInstance().getTime())));
-    }
-
     private void displayAuthFailedToast() {
         Toast.makeText(getApplicationContext(), "Authentication failed",
                 Toast.LENGTH_SHORT)
@@ -235,20 +221,5 @@ public class UserInfectionActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),
                 "Authentication succeeded!", Toast.LENGTH_SHORT).show();
         executeHealthStatusChange();
-    }
-
-    private void clickAction(Button button, TextView textView, int buttonText, int textViewText, int textColor) {
-        button.setText(buttonText);
-        textView.setTextColor(getResources().getColorStateList(textColor, this.getTheme()));
-        textView.setText(textViewText);
-    }
-
-    private void setView(Button button, TextView textView, boolean infected) {
-        int buttonText = infected ? R.string.i_am_cured : R.string.i_am_infected;
-        int textViewText = infected ? R.string.your_user_status_is_set_to_infected : R.string.your_user_status_is_set_to_not_infected;
-        int textColor = infected ? R.color.colorRedInfected : R.color.colorGreenCured;
-        button.setText(buttonText);
-        textView.setTextColor(getResources().getColorStateList(textColor, this.getTheme()));
-        textView.setText(textViewText);
     }
 }

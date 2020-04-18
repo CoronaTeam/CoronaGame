d0e70a588f4c9eda9eaa4e23d052599bd87e4331
lucieho <bamboomusic0@gmail.com>
2020-04-18 13:14:18 +0200

    create PathsFragment

---
+++ app/src/main/java/ch/epfl/sdp/Map/PathsFragment.java
@@ -0,0 +1,10 @@
+package ch.epfl.sdp.Map;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * This fragment is used to display the user's last positions as a line on the map, as well as points of met infected users
+ */
+public class PathsFragment extends Fragment {
+
+}

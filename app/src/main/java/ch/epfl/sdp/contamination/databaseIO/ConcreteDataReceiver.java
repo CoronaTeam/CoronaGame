12deb0c82bfd11a2ebf1a4bd7fdf586887734b96
matteopariset <matteopariset@gmail.com>
2020-05-17 16:51:16 +0200

    Refactor ConcreteDataReceiver

--- app/src/main/java/ch/epfl/sdp/contamination/databaseIO/ConcreteDataReceiver.java
+++ app/src/main/java/ch/epfl/sdp/contamination/databaseIO/ConcreteDataReceiver.java
@@ -21,14 +21,18 @@ import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 
 import ch.epfl.sdp.contamination.Carrier;
-import ch.epfl.sdp.contamination.Layman;
 import ch.epfl.sdp.contamination.Neighbor;
 import ch.epfl.sdp.identity.Account;
 
 import static ch.epfl.sdp.contamination.databaseIO.CachingDataSender.privateUserFolder;
 import static ch.epfl.sdp.contamination.databaseIO.CachingDataSender.publicUserFolder;
 import static ch.epfl.sdp.firestore.FirestoreInteractor.documentReference;
+import static ch.epfl.sdp.firestore.FirestoreInteractor.getTag;
 import static ch.epfl.sdp.firestore.FirestoreLabels.GEOPOINT_TAG;
+import static ch.epfl.sdp.firestore.FirestoreLabels.ILLNESS_PROBABILITY_TAG;
+import static ch.epfl.sdp.firestore.FirestoreLabels.INFECTION_STATUS_TAG;
+import static ch.epfl.sdp.firestore.FirestoreLabels.UNIQUE_ID_TAG;
+import static ch.epfl.sdp.firestore.FirestoreLabels.UNIXTIME_TAG;
 
 public class ConcreteDataReceiver implements DataReceiver {
 
@@ -52,11 +56,11 @@ public class ConcreteDataReceiver implements DataReceiver {
         return interactor.gridRead(location, date.getTime()).thenApply(stringMapMap -> {
             Set<Carrier> carriers = new HashSet<>();
             for (Map.Entry<String, Map<String, Object>> doc : stringMapMap.entrySet()) {
-                carriers.add((Carrier) doc);
-                carriers.add(new Layman(Enum.valueOf(Carrier.InfectionStatus.class,
-                        (String) doc.getValue().get("infectionStatus")),
-                        ((float) ((double) doc.getValue().get(
-                                "illnessProbability")))));
+                carriers.add(new Neighbor(Enum.valueOf(Carrier.InfectionStatus.class,
+                        getTag(doc.getValue(), INFECTION_STATUS_TAG, String.class)),
+                        getTag(doc.getValue(), ILLNESS_PROBABILITY_TAG, Double.class).floatValue(),
+                        getTag(doc.getValue(), UNIQUE_ID_TAG, String.class))
+                );
             }
             return carriers;
         }).exceptionally(exception -> Collections.emptySet());
@@ -65,12 +69,38 @@ public class ConcreteDataReceiver implements DataReceiver {
     private Set<Long> filterValidTimes(long startDate, long endDate, Map<String, Map<String, Object>> snapshot) {
         Set<Long> validTimes = new HashSet<>();
         for (Map.Entry<String, Map<String, Object>> q : snapshot.entrySet()) {
-            long time = Long.decode((String) q.getValue().get("Time"));
+            long time = Long.decode(getTag(q.getValue(), UNIXTIME_TAG, String.class));
+
             if (startDate <= time && time <= endDate) {
                 validTimes.add(time);
             }
         }
         return validTimes;
+    }
+
+    public Map<Carrier, Integer> collectCarriersMetDuringInterval(Stream<Map<String, Map<String, Object>>> results) {
+
+        Map<Carrier, Integer> metDuringInterval = new HashMap<>();
+        results.forEach(res -> {
+            for (Map.Entry<String, Map<String, Object>> doc : res.entrySet()) {
+                Carrier c = new Neighbor(
+                        Enum.valueOf(Carrier.InfectionStatus.class,
+                                getTag(doc.getValue(), INFECTION_STATUS_TAG, String.class)),
+                        getTag(doc.getValue(), ILLNESS_PROBABILITY_TAG, Double.class).floatValue(),
+                        getTag(doc.getValue(), UNIQUE_ID_TAG, String.class));
+
+                // TODO: [LOG]
+                Log.e("MET_DURING_INTERVAL", c.getInfectionStatus() + ", " + c.getIllnessProbability());
+
+                int numberOfMeetings = 1;
+                if (metDuringInterval.containsKey(c)) {
+                    numberOfMeetings += metDuringInterval.get(c);
+                }
+                metDuringInterval.put(c, numberOfMeetings);
+            }
+        });
+
+        return metDuringInterval;
     }
 
     @Override
@@ -79,47 +109,23 @@ public class ConcreteDataReceiver implements DataReceiver {
         return interactor.getTimes(location)
                 .thenApply(stringMapMap -> filterValidTimes(startDate.getTime(), endDate.getTime(), stringMapMap))
                 .thenApply(filtered -> {
-                    // TODO: Debug code
+                    // TODO: [LOG]
                     if (filtered.size() > 0) {
                         Log.e("FILTERED_TIMES", Long.toString(filtered.iterator().next()));
-                    } else {
-
                     }
                     return filtered;
                 })
                 .thenCompose(validTimes -> {
+                    // Retrieve all the Carrier met at each time
                     List<CompletableFuture<Map<String, Map<String, Object>>>> metDuringSlices = new ArrayList<>();
-
                     validTimes.forEach(tm -> metDuringSlices.add(interactor.gridRead(location, tm)));
 
-                    return CompletableFuture.allOf(metDuringSlices.toArray(new CompletableFuture[metDuringSlices.size()]))
-                            .thenApply(ignoredVoid -> {
+                    CompletableFuture<Void> carriersForTimeSlice = CompletableFuture.allOf(metDuringSlices.toArray(new CompletableFuture[metDuringSlices.size()]));
+
+                    return carriersForTimeSlice.thenApply(ignoredVoid -> {
+                                // Create a Map containing all the Carriers met during this interval
                                 Stream<Map<String, Map<String, Object>>> results = metDuringSlices.stream().map(ft -> ft.join());
-
-                                Map<Carrier, Integer> metDuringInterval = new HashMap<>();
-
-                                results.forEach(res -> {
-                                    for (Map.Entry<String, Map<String, Object>> doc : res.entrySet()) {
-                                        Carrier c = new Neighbor(
-                                                Enum.valueOf(Carrier.InfectionStatus.class,
-                                                        (String) doc.getValue().get("infectionStatus")),
-                                                ((float) ((double) doc.getValue().get("illnessProbability"))),
-                                                (String) doc.getValue().get("uniqueId"));
-
-                                        // TODO: Use Neighbor when appropriate
-
-                                        // TODO: Debug log
-                                        Log.e("MET_DURING_INTERVAL", c.getInfectionStatus() + ", " + c.getIllnessProbability());
-
-                                        int numberOfMeetings = 1;
-                                        if (metDuringInterval.containsKey(c)) {
-                                            numberOfMeetings += metDuringInterval.get(c);
-                                        }
-                                        metDuringInterval.put(c, numberOfMeetings);
-                                    }
-                                });
-
-                                return metDuringInterval;
+                                return collectCarriersMetDuringInterval(results);
                             });
                 })
                 .exceptionally(exception -> Collections.emptyMap());
@@ -129,7 +135,7 @@ public class ConcreteDataReceiver implements DataReceiver {
     public CompletableFuture<Location> getMyLastLocation(Account account) {
         return interactor.readLastLocation(account).thenApply(result -> {
             if (result.entrySet().iterator().hasNext()) {
-                GeoPoint geoPoint = (GeoPoint)result.get(GEOPOINT_TAG);
+                GeoPoint geoPoint = getTag(result, GEOPOINT_TAG, GeoPoint.class);
                 Location location = new Location(LocationManager.GPS_PROVIDER);
                 location.setLatitude(geoPoint.getLatitude());
                 location.setLongitude(geoPoint.getLongitude());

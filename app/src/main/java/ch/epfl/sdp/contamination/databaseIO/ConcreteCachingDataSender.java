0fe563e35a691c51e5907c027e0e62cf20097162
TheCrazyKing <lucas.meier@epfl.ch>
2020-05-22 08:24:40 +0200

    add position_history in sender

--- app/src/main/java/ch/epfl/sdp/contamination/databaseIO/ConcreteCachingDataSender.java
+++ app/src/main/java/ch/epfl/sdp/contamination/databaseIO/ConcreteCachingDataSender.java
@@ -6,15 +6,23 @@ import androidx.annotation.VisibleForTesting;
 
 import com.google.firebase.firestore.GeoPoint;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import ch.epfl.sdp.CoronaGame;
 import ch.epfl.sdp.contamination.Carrier;
 import ch.epfl.sdp.identity.fragment.AccountFragment;
+import ch.epfl.sdp.storage.ConcreteManager;
+import ch.epfl.sdp.storage.StorageManager;
 
 import static ch.epfl.sdp.firestore.FirestoreInteractor.documentReference;
 import static ch.epfl.sdp.firestore.FirestoreLabels.GEOPOINT_TAG;
@@ -30,12 +38,58 @@ public class ConcreteCachingDataSender implements CachingDataSender {
 
     SortedMap<Date, Location> lastPositions;
     private GridFirestoreInteractor gridInteractor;
+    private StorageManager<Date,GeoPoint> position_history;
 
     public ConcreteCachingDataSender(GridFirestoreInteractor interactor) {
         this.gridInteractor = interactor;
         this.lastPositions = new TreeMap<>();
+        this.position_history = initStorageManager();
     }
+    private StorageManager<Date, GeoPoint> openStorageManager() {
 
+        return new ConcreteManager<Date, GeoPoint>(
+                CoronaGame.getContext(),
+                "last_positions.csv",
+                date_position -> {
+                    try {
+                        return CoronaGame.dateFormat.parse(date_position);
+                    } catch (ParseException e) {
+                        throw new IllegalArgumentException("The file specified has wrong format: field 'date_position'");
+                    }
+                }, geoPoint->{
+
+                return stringToGeoPoint(geoPoint);
+                }
+        );
+    }
+    private GeoPoint stringToGeoPoint(String s){
+        Pattern p = Pattern.compile("-?\\d+(,\\d+)*?\\.?\\d+?");
+        List<Double> numbers = new ArrayList<Double>();
+        Matcher m = p.matcher(s);
+
+        while (m.find()) {
+            numbers.add(Double.valueOf(m.group()));
+        }
+        GeoPoint res;
+        try{
+            res = new GeoPoint(numbers.get(0),numbers.get(1));
+        }catch (NullPointerException e){
+            throw new IllegalArgumentException("The cache is not storing geoPoint" + e.getStackTrace());
+        }
+        return res;
+    }
+    // Load previous probabilities history
+    private StorageManager<Date, GeoPoint> initStorageManager() {
+
+        StorageManager<Date, GeoPoint> cm = openStorageManager();
+
+        if (!cm.isReadable()) {
+            cm.delete();
+            cm = openStorageManager();
+        }
+
+        return cm;
+    }
     @VisibleForTesting
     void setInteractor(GridFirestoreInteractor interactor) {
         this.gridInteractor = interactor;

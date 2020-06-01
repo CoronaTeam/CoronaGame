7f18d028f5fca5a2e265ff83e9bf7924b6359408
Adrien Remillieux <adrien.remillieux@epfl.ch>
2020-06-01 20:36:04 +0200

    deleted toDelete folder

--- app/src/androidTest/java/ch/epfl/sdp/map/fragment/MapActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/map/fragment/MapActivityTest.java
@@ -23,7 +23,6 @@ import ch.epfl.sdp.testActivities.MapActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.sdp.TestTools.sleep;
 import static ch.epfl.sdp.location.LocationUtils.buildLocation;
 import static ch.epfl.sdp.map.HeatMapHandler.HEATMAP_LAYER_ID;
@@ -31,7 +30,6 @@ import static com.mapbox.mapboxsdk.style.layers.Property.NONE;
 import static com.mapbox.mapboxsdk.style.layers.Property.VISIBLE;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
-import static junit.framework.TestCase.assertTrue;
 
 public class MapActivityTest {
 

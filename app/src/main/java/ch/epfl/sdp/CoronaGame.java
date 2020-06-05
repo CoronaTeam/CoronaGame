b1cb18471acf409844a53acbfbf586f85ea1a8c1
TheBlueHawk <kblin@outlook.com>
2020-06-05 22:06:14 +0200

    reduce speedup

--- app/src/main/java/ch/epfl/sdp/CoronaGame.java
+++ app/src/main/java/ch/epfl/sdp/CoronaGame.java
@@ -35,7 +35,7 @@ public class CoronaGame extends Application implements Application.ActivityLifec
     public static boolean IS_ONLINE = TRUE;
     public static boolean IS_NETWORK_DEBUG = FALSE;
     private static Context context;
-    private static int DEMO_SPEEDUP = 100;
+    private static int DEMO_SPEEDUP = 25;
     private Activity currentActivity;
 
     public static int getDemoSpeedup() {
